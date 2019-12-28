68e0e19d9520cc91364ae1a2ac572b3dd6fb08ff
rvarszegi <r.varszegi93@gmail.com>
2019-12-28 16:46:53 +0100

    finished with lecture 122.

--- recipe-book/src/app/recipes/recipe.model.ts
+++ recipe-book/src/app/recipes/recipe.model.ts
@@ -1,11 +1,17 @@
+import {Ingredient} from '../shared/ingredient.model';
+import {Injectable} from '@angular/core';
+
+@Injectable()
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, desc: string, imagePath: string) {
+  constructor(name: string, desc: string, imagePath: string, ingredient: Ingredient[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.ingredients = ingredient;
   }
 }
